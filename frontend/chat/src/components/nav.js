919302374f2d64ffd29d20ddf775ea3a636e684e
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-08-19 00:24:51 +0000

    WORKING on login form

--- frontend/chat/src/components/nav.js
+++ frontend/chat/src/components/nav.js
@@ -1,38 +1,29 @@
+"use client"
+import { useRouter } from "next/navigation"
+
 export default function Nav(params) {
+
+  const  router = useRouter()
     return (
 
         <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Chat</a>
+          <a class="navbar-brand" onClick={()=>{router.push(`/`)}}>Chat</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarColor01">
             <ul class="navbar-nav me-auto">
               <li class="nav-item">
-                <a class="nav-link active" href="#">Home
-                  <span class="visually-hidden">(current)</span>
-                </a>
+                <a class="nav-link" onClick={()=>{router.push(`/auth/login`)}}>Login</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Login</a>
+                <a class="nav-link" onClick={()=>{router.push(`/auth/register`)}}>Register</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Register</a>
+                <a class="nav-link" onClick={()=>{router.push(`/about`)}}>About Us</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">About Us</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                <div class="dropdown-menu">
-                  <a class="dropdown-item" href="#">Action</a>
-                  <a class="dropdown-item" href="#">Another action</a>
-                  <a class="dropdown-item" href="#">Something else here</a>
-                  <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="#">Separated link</a>
-                </div>
-              </li>
+
             </ul>
             <form class="d-flex">
               <input class="form-control me-sm-2" type="search" placeholder="Search"/>
