bc8379c46e4894a7ac0c5074471cedcc1c7a5a47
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-08-19 20:01:09 +0000

    DONE form login and register

--- frontend/chat/src/app/auth/login/page.jsx
+++ frontend/chat/src/app/auth/login/page.jsx
@@ -1,24 +1,46 @@
+"use client"
+
+import { useState } from "react";
+
 export default function Login() {
+  const [email,setEmial] = useState("")
+  const [password,setPassword] = useState("")
+
+
+
+  function handleSubmit() {
+    event.preventDefault();
+    alert(email +" "+ password)    
+  }
     return(  
-    <form>
+    <form onSubmit={handleSubmit}>
     <fieldset>
     <legend>Login</legend>
-    <div class="form-group row">
-    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-    <div class="col-sm-10">
-    <input type="text" readonly="" class="form-control-plaintext" id="staticEmail" value="email@example.com"/>
+
+    <div className="form-group">    
+    <label htmlFor="exampleInputEmail1" className="form-label mt-4">Email address</label>
+      <input 
+        type="email" 
+        className="form-control" 
+        id="exampleInputEmail1" 
+        value={email}
+        onChange={(e) => setEmial(e.target.value)}
+        aria-describedby="emailHelp" 
+        placeholder="Enter email"/>
     </div>
+    
+    <div className="form-group">
+    <label htmlFor="exampleInputPassword1" className="form-label mt-4">Password</label>
+      <input 
+        type="password" 
+        className="form-control" 
+        id="exampleInputPassword1"
+        value={password} 
+        onChange={(e)=> { setPassword(e.target.value)}}
+        placeholder="Password" 
+        autoComplete="off"/>
     </div>
-    <div class="form-group">
-      <label for="exampleInputEmail1" class="form-label mt-4">Email address</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-    </div>
-    <div class="form-group">
-      <label for="exampleInputPassword1" class="form-label mt-4">Password</label>
-      <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" autocomplete="off"/>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" className="btn btn-primary">Submit</button>
   </fieldset>
 </form>
      
