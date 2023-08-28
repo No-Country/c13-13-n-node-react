93a15f2f7b01c001a7eccc8ee4c095f1c57dc9ee
JaviPeder~ <javierpedernera@gmail.com>
2023-08-27 22:58:20 -0300

    modificaciones en la pagina principal-animacion

--- frontend/chat/src/components/nav.js
+++ frontend/chat/src/components/nav.js
@@ -9,12 +9,11 @@ export default function Nav(params) {
 
         <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
         <div className="container-fluid">
-<div style={{display:"flex align-center" ,flexGrow: "nowrap",flexDirection: "row",alignItems: "center", color: "white", fontSize:"20px" }}>
+<div style={{display:"flex" ,flexWrap: "nowrap",flexDirection: "row",alignItems: "center", color: "white", fontSize:"20px" }}>
         <Link className="navbar-brand" href="/" >
           <img src="https://res.cloudinary.com/dbwmesg3e/image/upload/v1693015077/NoCountry/image_o8sh38.png" alt="Logo" width="60" padding-left="20px" height="60"  className="d-inline-block align-center" style={{ marginLeft: "15%" }}/>  
-        </Link>
-        
-          <h2 style={{ color: "white", marginLeft: "5px", fontSize:"20px" }}>Tell me</h2>
+        </Link>       
+        <div style={{ color: "white", marginLeft: "5px", fontSize:"1.8rem", fontFamily: "'Lobster Two', cursive" }}>Tell me</div>
 </div>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -27,6 +26,7 @@ export default function Nav(params) {
               <li className="nav-item">
                 <a className="nav-link" onClick={()=>{router.push(`/auth/register`)}}>Register</a>
               </li>
+              
               <li className="nav-item">
                 <a className="nav-link" onClick={()=>{router.push(`/about`)}}>About Us</a>
               </li>
