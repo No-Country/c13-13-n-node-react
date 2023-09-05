f55cd9fb5bb1b70fc99fcd4aa03679f24991cc3a
Diego Dimitroff Petcoff <82569859+DiegoDimitroffPetcoff@users.noreply.github.com>
2023-09-05 19:52:06 +0000

    UNLOCKED dbs informarion

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,8 @@ const { conn } = require("./src/db.js");
 // Sincronización de todos los modelos:
 const port = process.env.PORT || 8080;
 
-//conn.sync({ force: false }).then(() => {
+conn.sync({ force: false }).then(() => {
   app.listen(port, async () => {
     console.log(`%s listening at ${port}`);
   });
-//});
+});
