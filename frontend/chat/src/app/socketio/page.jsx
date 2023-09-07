ec42ad37d7d5fda8c09a536ed4ae53f0377ddac4
Diego Dimitroff Petcoff <diegodimitroffpetcoff@gmail.com>
2023-09-07 13:57:59 +0100

    COMING BACK to the commit witch works propertly

--- frontend/chat/src/app/socketio/page.jsx
+++ frontend/chat/src/app/socketio/page.jsx
@@ -1,69 +1,50 @@
 "use client";
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
-const socket = io.connect ("http://localhost:8080")
+const socket = io.connect("http://localhost:8080");
 
 export default function Socket() {
   const [message, setMessage] = useState("");
   const [messageReceived, setMessageReceived] = useState("");
+  const [historial, setHistorial] = useState([]);
 
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("newMessage", message);
+  };
 
-  /**useEffect(()=>{
-        socket.on("receive_message",(data)=>{
-            alert(data.message)
-        })
-    },[socket]) */
-
-  /**How it should be on the backend
-    io.on("connection", (socket)=>{
-        console.log(`User Connected: ${socket.io}`);
-
-     socket.on("send_message",(data)=>{
-        de esta manera se envia el mensaje a todos los conectados:
-        socket.broadcast.emit("receive_message, data")
-
-        console.log(data)  este seria el mensaje recibido en el back del front
-        })
-    })*/
-
-
-    const handleSubmit = (e) =>{
-      e.preventDefault()     
-      socket.emit("newMessage",message)
-
-
-    }
-
-    useEffect(()=>{
-      socket.on("message",(message)=>{     
-        setMessageReceived(message)
-    })
-,[socket]    })
-
-
+  useEffect(() => {
+    socket.on("message", (message) => {
+      // Añadir el nuevo mensaje al historial
+      setHistorial(prevHistorial => [...prevHistorial, message]);
+      // Actualizar el mensaje recibido
+      setMessageReceived(message);
+    });
+  }, []);
 
   return (
     <div className="contarinerGral">
-      <div className="winsowChat">
-
+     <div className="winsowChat">
         <h5>{messageReceived}</h5>
+        {/* Mostrar el historial de mensajes */}
+        {historial.map((mensaje, index) => (
+          <div key={index}>{mensaje}</div>
+        ))}
       </div>
 
       <form onSubmit={handleSubmit}>
-      <input
-        className="form-control"
-        id="disabledInput"
-        type="text"
-        placeholder="Mensaje.."
-        value={message}
-        onChange={(e) => {
-          setMessage(e.target.value);
-        }}
-      />
-      <button className="btn btn-primary">
-        Enviar mensaje
-      </button>
+        <input
+          className="form-control"
+          id="disabledInput"
+          type="text"
+          placeholder="Mensaje.."
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+        />
+        <button className="btn btn-primary">Enviar mensaje</button>
       </form>
     </div>
   );
