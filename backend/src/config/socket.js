10034ddcaf9e2fa49f38296dc6c18fe1c1bddd3f
JaviPeder~ <javierpedernera@gmail.com>
2023-09-13 22:46:44 -0300

    probando socket

--- backend/src/config/socket.js
+++ backend/src/config/socket.js
@@ -12,24 +12,22 @@ module.exports = (io) => {
     io.on('connection', (socket) => {
       console.log('Usuario conectado: ' + socket.id);
   
-      // socket.broadcast.emit('chat_message', {
-      //     usuario: 'INFO',
-      //     mensaje: 'Se ha conectado un usuario'
-      // });
-      socket.on("join_room",  ({ username, roomId }) => {
-            socket.join(roomId); // Unirse a la sala
-            // Puedes enviar un mensaje o emitir un evento para notificar a los otros usuarios que alguien se unió a la sala
-            io.to(roomId).emit('user_connected_notice', {mensaje:`${username} se ha unido a la sala.`});
-          });
+      socket.broadcast.emit('chat_message', {
+          usuario: 'INFO',
+          mensaje: 'Se ha conectado un usuario'
+      });
+  
       socket.on('chat_message', (data) => {
         const {room, usuario, mensaje } = data;
         io.to(room).emit('chat_message', data);
       });
 
-      
-      socket.on('disconnect', () => {
-        console.log('Un usuario se ha desconectado.');
+      socket.on("join_room",  ({ username, roomId }) => {
+        socket.join(roomId); // Unirse a la sala
+        // Puedes enviar un mensaje o emitir un evento para notificar a los otros usuarios que alguien se unió a la sala
+        io.to(roomId).emit("user_joined", `${username} se unió a la sala.`);
       });
+
 
 
   });
